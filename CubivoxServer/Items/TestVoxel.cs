12899cbe682035721db6c5b7c1c1af05dd02dcb3
Ryandw11 <6239385+ryandw11@users.noreply.github.com>
2024-05-19 14:32:37 -0700

    Remove Test Code

--- CubivoxServer/Items/TestVoxel.cs
+++ CubivoxServer/Items/TestVoxel.cs
@@ -1,9 +1,5 @@
-﻿using CubivoxCore;
-using CubivoxCore.Attributes;
-using CubivoxCore.Chat;
-using CubivoxCore.Events;
+﻿using CubivoxCore.Attributes;
 using CubivoxCore.Mods;
-using CubivoxCore.Players;
 
 namespace CubivoxServer.Items
 {
@@ -14,35 +10,6 @@ namespace CubivoxServer.Items
     {
         public TestVoxel(Mod mod) : base(mod)
         {
-            PlaceEvent += OnVoxelPlace;
-            BreakEvent += OnVoxelBreak;
-            Console.WriteLine("DONE!");
-        }
-
-        [ServerOnly]
-        private void OnVoxelPlace(VoxelDefPlaceEvent evt)
-        {
-            /*Location loc = evt.Location;
-            if (loc.x > 0)
-            {
-                evt.Player.SendMessage("Permission Denied. Cannot place a voxel in this area.".Color("red"));
-                evt.Cancel();
-                return;
-            }
-            Console.WriteLine($"{evt.Player.GetName()} has placed the test voxel!");
-            evt.Player.SendMessage("Mmm, nice block you placed there at ".Color("red") + evt.Location.ToString().Color("green") + "!".Color("red"));*/
-        }
-
-        [ServerOnly]
-        private void OnVoxelBreak(VoxelDefBreakEvent evt)
-        {
-            /*Location loc = evt.Location;
-            if (loc.x > 0)
-            {
-                evt.Player.SendMessage("Permission Denied. Cannot break a voxel in this area.".Color("red"));
-                evt.Cancel();
-                return;
-            }*/
         }
     }
 }
